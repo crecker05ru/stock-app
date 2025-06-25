225154817bdb0d9b3a601f039dd1aceca5bdc191
anvar <fort2652@rambler.ru>
2025-06-25 15:37:29 +0300

    add modes

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,23 @@ const worker = new Worker('./webWorker.js')
 // const serviceWorker = import('./serviceWorker.js')
 worker.postMessage({ message: 'connect webworker' })
 // sharedWorker.port.postMessage('connect shared worker')
+
+function isSharedWorkerSupported() {
+  try {
+    // Проверяем наличие SharedWorker и пробуем создать временный воркер
+    return typeof SharedWorker === 'function' && !!new SharedWorker('data:application/javascript,')
+  } catch (e) {
+    return false
+  }
+}
+
+if (isSharedWorkerSupported()) {
+  const sharedWorker = new SharedWorker('./sharedWorker.js')
+  sharedWorker.port.postMessage('connect shared worker')
+} else {
+  console.error('SharedWorker не поддерживается или заблокирован')
+}
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,34 +8,46 @@ import VueMacros from 'vue-macros/vite'
 import Inspect from 'vite-plugin-inspect'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.APP_BASE,
-  assetsInclude: ['**/'],
-  plugins: [
-    // vue(),
-    // vueJsx(),
-    vueDevTools(),
-    VueMacros({
-      plugins: {
-        vue: vue(),
-        vueJsx: vueJsx(),
-      },
-    }),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: { __APP_ENV__: JSON.stringify(env.APP_ENV) },
+    base: process.env.APP_BASE,
+    assetsInclude: ['**/'],
+    plugins: [
+      // vue(),
+      // vueJsx(),
+      vueDevTools(),
+      VueMacros({
+        plugins: {
+          vue: vue(),
+          vueJsx: vueJsx(),
+        },
+      }),
 
-    Inspect(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@/assets/styles/mixins.scss";
-        `,
+      Inspect(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/styles/mixins.scss";
+          `,
+        },
+      },
+    },
+    build: {
+      outDir: mode === 'production' ? 'dist' : mode === 'demo' ? 'dist' : 'dist',
+      chunkSizeWarningLimit: 2500,
+      // rollupOptions: {
+      //   output: {
+      //   }
+      // }
+    },
+  }
 })
