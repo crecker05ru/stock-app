8009238ae917bf539e67063f5b31b2261e424d89
anvar <fort2652@rambler.ru>
2025-06-25 12:00:22 +0300

    add https server 4

--- server/server.js
+++ server/server.js
@@ -183,15 +183,15 @@ app.use(bodyParser.json())
 //   next()
 // }
 
-app.enable('trust proxy') // Важно для корректного определения HTTPS
+// app.enable('trust proxy') // Важно для корректного определения HTTPS
 
-app.use((req, res, next) => {
-  if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
-    next()
-  } else {
-    res.redirect(`https://${req.headers.host}${req.url}`)
-  }
-})
+// app.use((req, res, next) => {
+//   if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
+//     next()
+//   } else {
+//     res.redirect(`https://${req.headers.host}${req.url}`)
+//   }
+// })
 
 // app.configure(() => {
 //   app.use(express.bodyParser())
