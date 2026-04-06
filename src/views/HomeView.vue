ac3c4f0af839d53c0c2d5c9163f9f8bd51ec8cf8
anvar <fort2652@rambler.ru>
2026-04-06 23:01:26 +0300

    server-config: add tg app 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,8 +57,8 @@
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
-import TheWelcome from '../components/TheWelcome.vue'
-import FilterBlock from '../components/FilterBlock.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import FilterBlock from '@/components/FilterBlock.vue'
 import StockItem from '@/components/ui/StockItem.vue'
 import IconArrowRight from '@/components/icons/IconArrowRight.vue'
 import { useUserStore } from '@/stores/user.ts'
